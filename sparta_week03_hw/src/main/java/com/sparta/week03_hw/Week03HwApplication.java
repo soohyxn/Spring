822678b68d638ba4cc7aaae90a5126e18cc07638
soohyun <soohyun6879@naver.com>
2021-09-08 16:56:49 +0900

    sparta week03_hw

---
+++ sparta_week03_hw/src/main/java/com/sparta/week03_hw/Week03HwApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.week03_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week03HwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week03HwApplication.class, args);
+    }
+
+}
---
+++ sparta_week03_hw/src/main/java/com/sparta/week03_hw/domain/MemoRequestDto.java
@@ -0,0 +1,9 @@
+package com.sparta.week03_hw.domain;
+
+import lombok.Getter;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String contents;
+}
