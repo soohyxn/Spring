f84841ddf0be94157131ec69f9d7a40a3c2b996f
soohyun <soohyun6879@naver.com>
2021-10-01 14:21:46 +0900

    sparta week07_hw

---
+++ sparta_week07_hw/src/main/java/com/sparta/week06/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.week06.repository;
+
+import com.sparta.week06.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByUserId(Long userId);
+}
