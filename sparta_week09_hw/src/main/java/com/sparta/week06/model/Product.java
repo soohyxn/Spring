a5da4e65681f1943e26043d77b1c88c97683933e
soohyun <soohyun6879@naver.com>
2021-10-15 18:44:31 +0900

    sparta week09_hw

---
+++ sparta_week09_hw/src/main/java/com/sparta/week06/model/Product.java
@@ -0,0 +1,83 @@
+package com.sparta.week06.model;
+
+import com.sparta.week06.dto.ProductRequestDto;
+import com.sparta.week06.util.URLValidator;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Setter
+@Getter // get 함수를 일괄적으로 만들어줍니다.
+@NoArgsConstructor // 기본 생성자를 만들어줍니다.
+@Entity // DB 테이블 역할을 합니다.
+public class Product extends Timestamped {
+    // ID가 자동으로 생성 및 증가합니다.
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+
+    // 반드시 값을 가지도록 합니다.
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String link;
+
+    @Column(nullable = false)
+    private int lprice;
+
+    @Column(nullable = false)
+    private int myprice;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @ManyToMany
+    private List<Folder> folderList;
+
+    public Product(ProductRequestDto requestDto, Long userId) {
+        // 입력값 Validation
+        if (userId == null || userId < 0) {
+            throw new IllegalArgumentException("회원 Id 가 유효하지 않습니다.");
+        }
+
+        if (requestDto.getTitle() == null || requestDto.getTitle().isEmpty()) {
+            throw new IllegalArgumentException("저장할 수 있는 상품명이 없습니다.");
+        }
+
+        if (!URLValidator.urlValidator(requestDto.getImage())) {
+            throw new IllegalArgumentException("상품 이미지 URL 포맷이 맞지 않습니다.");
+        }
+
+        if (!URLValidator.urlValidator(requestDto.getLink())) {
+            throw new IllegalArgumentException("상품 최저가 페이지 URL 포맷이 맞지 않습니다.");
+        }
+
+        if (requestDto.getLprice() <= 0) {
+            throw new IllegalArgumentException("상품 최저가가 0 이하입니다.");
+        }
+
+        // 관심상품을 등록한 회원 Id 저장
+        this.userId = userId;
+        this.title = requestDto.getTitle();
+        this.image = requestDto.getImage();
+        this.link = requestDto.getLink();
+        this.lprice = requestDto.getLprice();
+        this.myprice = 0;
+    }
+
+    // 관심 상품의 가격 변경 시 사용합니다.
+    public void updateMyPrice(int price) {
+        this.myprice = price;
+    }
+
+    public void addFolder(Folder folder) {
+        this.folderList.add(folder);
+    }
+}
---
+++ sparta_week09_hw/src/main/java/com/sparta/week06/service/ProductService.java
@@ -0,0 +1,101 @@
+package com.sparta.week06.service;
+
+import com.sparta.week06.model.Folder;
+import com.sparta.week06.model.Product;
+import com.sparta.week06.model.User;
+import com.sparta.week06.repository.FolderRepository;
+import com.sparta.week06.repository.ProductRepository;
+import com.sparta.week06.dto.ProductMypriceRequestDto;
+import com.sparta.week06.dto.ProductRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+import java.util.List;
+@Service
+public class ProductService {
+    // 멤버 변수 선언
+    private final ProductRepository productRepository;
+    private final FolderRepository folderRepository;
+    private static final int MIN_PRICE = 100;
+
+    // 생성자: ProductService() 가 생성될 때 호출됨
+    @Autowired
+    public ProductService(ProductRepository productRepository, FolderRepository folderRepository) {
+        // 멤버 변수 생성
+        this.productRepository = productRepository;
+        this.folderRepository = folderRepository;
+    }
+
+    public Page<Product> getProducts(Long userId, int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return productRepository.findAllByUserId(userId, pageable);
+    }
+
+    @Transactional // 메소드 동작이 SQL 쿼리문임을 선언합니다.
+    public Product createProduct(ProductRequestDto requestDto, Long userId) {
+        // 요청받은 DTO 로 DB에 저장할 객체 만들기
+        Product product = new Product(requestDto, userId);
+        productRepository.save(product);
+        return product;
+    }
+
+    @Transactional // 메소드 동작이 SQL 쿼리문임을 선언합니다.
+    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
+        );
+
+        // 변경될 관심 가격이 유효한지 확인합니다.
+        int myPrice = requestDto.getMyprice();
+        if (myPrice < MIN_PRICE) {
+            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_PRICE + " 원 이상으로 설정해 주세요.");
+        }
+
+        product.updateMyPrice(myPrice);
+        return product;
+    }
+
+    // 모든 상품 조회 (관리자용)
+    public Page<Product> getAllProducts(int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return productRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public Product addFolder(Long productId, Long folderId, User user) {
+        // 1) 상품를 조회합니다.
+        Product product = productRepository.findById(productId).orElseThrow(
+                () -> new NullPointerException("해당 관심상품 아이디가 존재하지 않습니다.")
+        );
+
+        // 2) 관심상품을 조회합니다.
+        Folder folder = folderRepository.findById(folderId).orElseThrow(
+                () -> new NullPointerException("해당 폴더 아이디가 존재하지 않습니다.")
+        );
+
+        // 3) 조회한 폴더와 관심상품이 모두 로그인한 회원의 소유인지 확인합니다.
+        Long userId = user.getId();
+        Long productUserId = product.getUserId();
+        Long folderUserId = folder.getUser().getId();
+
+        if (!userId.equals(productUserId) || !userId.equals(folderUserId)) {
+            throw new IllegalArgumentException("회원님의 관심상품이 아니거나, 폴더가 아니어서 추가하지 못했습니다.");
+        }
+
+        // 4) 상품에 폴더를 추가합니다.
+        product.addFolder(folder);
+        return product;
+    }
+}
