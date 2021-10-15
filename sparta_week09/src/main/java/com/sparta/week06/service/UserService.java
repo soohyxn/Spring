faa90fe14b477ca0b6a2c7cdd583e3d0341fd2a8
soohyun <soohyun6879@naver.com>
2021-10-15 18:29:05 +0900

    sparta week09

---
+++ sparta_week09/src/main/java/com/sparta/week06/service/UserService.java
@@ -0,0 +1,99 @@
+package com.sparta.week06.service;
+
+import com.sparta.week06.dto.SignupRequestDto;
+import com.sparta.week06.kakao.KakaoOAuth2;
+import com.sparta.week06.kakao.KakaoUserInfo;
+import com.sparta.week06.model.User;
+import com.sparta.week06.model.UserRole;
+import com.sparta.week06.repository.UserRepository;
+import com.sparta.week06.security.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final KakaoOAuth2 kakaoOAuth2;
+    private final AuthenticationManager authenticationManager;
+    private static final String ADMIN_TOKEN = "AAABnv/xRVklrnYxKZ0aHgTBcXukeZygoC";
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, KakaoOAuth2 kakaoOAuth2, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.kakaoOAuth2 = kakaoOAuth2;
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    public User registerUser(SignupRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        // 회원 ID 중복 확인
+        Optional<User> found = userRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
+        }
+
+        // 패스워드 인코딩
+        String password = passwordEncoder.encode(requestDto.getPassword());
+        String email = requestDto.getEmail();
+        // 사용자 ROLE 확인
+        UserRole role = UserRole.USER;
+        if (requestDto.isAdmin()) {
+            if (!requestDto.getAdminToken().equals(ADMIN_TOKEN)) {
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
+            }
+            role = UserRole.ADMIN;
+        }
+
+        User user = new User(username, password, email, role);
+        userRepository.save(user);
+        return user;
+    }
+
+    public void kakaoLogin(String authorizedCode) {
+        // 카카오 OAuth2 를 통해 카카오 사용자 정보 조회
+        KakaoUserInfo userInfo = kakaoOAuth2.getUserInfo(authorizedCode);
+        Long kakaoId = userInfo.getId();
+        String nickname = userInfo.getNickname();
+        String email = userInfo.getEmail();
+
+        // DB 에 중복된 Kakao Id 가 있는지 확인
+        User kakaoUser = userRepository.findByKakaoId(kakaoId).orElse(null);
+
+        // 카카오 정보로 회원가입
+        if (kakaoUser == null) {
+            // 카카오 이메일과 동일한 이메일을 가지고 있는지 확인
+            User existUser = userRepository.findByEmail(email).orElse(null);
+
+            if(existUser != null) { // 카카오와 동일한 이메일이 있는 경우 kakaoId를 회원정보에 저장
+                kakaoUser = existUser;
+                kakaoUser.setKakaoId(kakaoId);
+                userRepository.save(kakaoUser);
+            } else { // 카카오와 동일한 이메일이 없는 경우 kakao 정보로 회원가입
+                // password = 카카오 Id + ADMIN TOKEN
+                String password = kakaoId + ADMIN_TOKEN;
+                // 패스워드 인코딩
+                String encodedPassword = passwordEncoder.encode(password);
+                // ROLE = 사용자
+                UserRole role = UserRole.USER;
+
+                kakaoUser = new User(nickname, encodedPassword, email, role, kakaoId);
+                userRepository.save(kakaoUser);
+            }
+        }
+
+        // 스프링 시큐리티 통해 로그인 처리
+        UserDetailsImpl userDetails = new UserDetailsImpl(kakaoUser);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
